fb992b6e936ebad7ad4c793990ac404b15fee010
“2556575” <2556575@students.wits.ac.za>
2024-04-10 18:11:47 +0200

    feat: add API

--- src/index.js
+++ src/index.js
@@ -1,52 +1,38 @@
-//create cars api using express
-const express = require('express');
-const app = express();
+// Fetch all cars
+fetch('/api/getAllCars')
+    .then(response => response.json())
+    .then(data => console.log(data));
 
+// Fetch a car by ID
+fetch('/api/getCarById?id=1')
+    .then(response => response.json())
+    .then(data => console.log(data));
 
+// Add a new car
+fetch('/api/addCar', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newCar),
+})
+.then(response => response.json())
+.then(data => console.log(data));
 
-app.use(express.json());
+// Update a car
+fetch('/api/updateCar', {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatedCar),
+})
+.then(response => response.json())
+.then(data => console.log(data));
 
-const cars = require('./cars.json');
-
-//get all cars
-app.get('/cars', (req, res) => {
-    res.json(cars);
-});
-
-//get car by id
-app.get('/cars/:id', (req, res) => {
-    const id = req.params.id;
-    const car = cars.find(car => car.id === id);
-    res.json(car);
-});
-
-//update car
-app.put('/cars/:id', (req, res) => {
-    const id = req.params.id;
-    const updatedCar = req.body;
-    const index = cars.findIndex(car => car.id === id);
-    cars[index] = updatedCar;
-    res.json(updatedCar);
-});
-
-//delete car
-app.delete('/cars/:id', (req, res) => {
-    const id = req.params.id;
-    const index = cars.findIndex(car => car.id === id);
-    cars.splice(index, 1);
-    res.json({ message: `Car with id ${id} deleted` });
-});
-
-//add car
-app.post('/cars', (req, res) => {
-    console.log(req);
-    const newCar = req.body;
-    console.log(newCar);
-    cars.push(newCar);
-    res.json(newCar);
-});
-
-//start app at localhost:3001
-app.listen(3001, () => {
-    console.log('Server started at http://localhost:3001');
-});
+// Delete a car
+fetch('/api/deleteCar?id=1', {
+    method: 'DELETE',
+})
+.then(response => response.json())
+.then(data => console.log(data));
